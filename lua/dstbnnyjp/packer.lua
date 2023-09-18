f1138cafc4238a5f336016df5123cf86822ce590
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2023-09-18 17:48:12 -0600

    chore: categorise plugins in packer

--- lua/dstbnnyjp/packer.lua
+++ lua/dstbnnyjp/packer.lua
@@ -5,10 +5,7 @@ return require("packer").startup(function(use)
     -- packer can manage itself
     use "wbthomason/packer.nvim"
 
-    use {
-        "nvim-telescope/telescope.nvim", tag = "0.1.1",
-        requires = { { "nvim-lua/plenary.nvim" } }
-    }
+    ---- LSP + PARSING ----
 
     use {
         "VonHeikemen/lsp-zero.nvim",
@@ -30,23 +27,36 @@ return require("packer").startup(function(use)
             { "rafamadriz/friendly-snippets" },
         }
     }
-
+    
+    -- to use non-LSPs w/ mason
     use "jose-elias-alvarez/null-ls.nvim"
     use "jay-babu/mason-null-ls.nvim"
 
+    -- parsing for context highlighting
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+
+    ---- INTERFACE ----
+
+    use "nvim-lualine/lualine.nvim"
     use "folke/trouble.nvim"
 
-    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    use {
+        "nvim-telescope/telescope.nvim", tag = "0.1.1",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
 
     use "mbbill/undotree"
 
-    use "nvim-lualine/lualine.nvim"
+    ---- UTILITY ----
 
+    -- git integration
     use "airblade/vim-gitgutter"
     use "tpope/vim-fugitive"
+
+    -- make life easier
     use "tpope/vim-commentary"
     use "tpope/vim-surround"
-
+    
     use "folke/zen-mode.nvim"
 
     -- pretty colours :3c
