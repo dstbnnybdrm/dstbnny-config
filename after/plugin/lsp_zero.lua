6a472e810251725e06d26460b67266e8f664d918
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2023-12-24 16:48:06 -0700

    fix(lsp_zero): change "prettier" to "prettierd" and other stuff i forget oops

--- after/plugin/lsp_zero.lua
+++ after/plugin/lsp_zero.lua
@@ -1,24 +1,24 @@
 ---- LSP ----
 
 -- use "minimal" preset
-local lsp = require("lsp-zero").preset({})
+local lsp_zero = require("lsp-zero").preset({})
 
-lsp.on_attach(function(client, bufnr)
+lsp_zero.on_attach(function(client, bufnr)
     -- set keymaps
     local opts = {buffer = bufnr, remap = false}
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp_zero.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp_zero.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp_zero.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp_zero.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp_zero.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp_zero.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp_zero.buf.signature_help() end, opts)
 end)
 
-lsp.format_on_save({
+lsp_zero.format_on_save({
     format_opts = {
         async = false,
         timeout_ms = 10000,
@@ -28,13 +28,22 @@ lsp.format_on_save({
     },
 })
 
-lsp.setup()
+lsp_zero.setup()
 
-local null_ls = require('null-ls')
+require("mason").setup({})
+require("mason-lspconfig").setup({
+  ensure_installed = {"html", "cssls", "eslint"},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
 
+require("mason-null-ls").setup({})
+
+local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettierd,
   }
 })
 
