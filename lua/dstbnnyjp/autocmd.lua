036aefd45caf763a1935ce816d6574ce43a66beb
dstbnnybdrm <dustbunny.bedroom@gmail.com>
2024-03-26 09:22:34 -0600

    refactor(autocmd): move autocmds to separate file

---
+++ lua/dstbnnyjp/autocmd.lua
@@ -0,0 +1,9 @@
+-- highlight yanks
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function() vim.highlight.on_yank { timeout = 500 } end
+})
+
+-- show diagnostic on hover
+vim.diagnostic.config({ virtual_text = false })
+vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float()]]
+vim.cmd [[autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()]]
--- lua/dstbnnyjp/init.lua
+++ lua/dstbnnyjp/init.lua
@@ -1,5 +1,6 @@
 require("dstbnnyjp.remap")
 require("dstbnnyjp.set")
+require("dstbnnyjp.autocmd")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -14,15 +15,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("dstbnnyjp.plugins")
-
--- highlight yanks
-vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function() vim.highlight.on_yank { timeout = 500 } end
-})
-
--- show diagnostic on hover
-vim.diagnostic.config({ virtual_text = false })
-vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float()]]
-vim.cmd [[autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()]]
 
 vim.g.netrw_banner = 0
