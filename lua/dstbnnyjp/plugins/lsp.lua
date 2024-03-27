ea7284b1a2cb544053d4e395c6780755ffcf87b3
dstbnnyjp <dustbunnybedroommusic@gmail.com>
2024-03-26 21:01:49 -0600

    feat(cmp): change select to false

--- lua/dstbnnyjp/plugins/lsp.lua
+++ lua/dstbnnyjp/plugins/lsp.lua
@@ -49,7 +49,7 @@ return {
                 },
                 mapping = cmp.mapping.preset.insert({
                     ["<C-Space>"] = cmp.mapping.complete(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
                     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-d>"] = cmp.mapping.scroll_docs(4),
                     ["<C-f>"] = cmp_action.luasnip_jump_forward(),
