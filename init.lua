a73fe62f47b0a0220f12ef94619ee12ca078f617
dstbnnyjp <dustbunny.bedroom@gmail.com>
2025-10-14 15:47:33 -0600

    feat(pack-man): switch from lazy to mini.deps

--- init.lua
+++ init.lua
@@ -1,2 +1,26 @@
-require("dstbnnyjp")
+require("core.opt")
+require("core.map")
+
+-- INSTALLING MINI.DEPS PACKAGE MANAGER ========================================
+
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.deps'
+if not vim.loop.fs_stat(mini_path) then
+    vim.cmd('echo "Installing `mini.deps`" | redraw')
+    local clone_cmd = {
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/nvim-mini/mini.deps", mini_path
+    }
+    vim.fn.system(clone_cmd)
+    vim.cmd("packadd mini.deps | helptags ALL")
+    vim.cmd('echo "Installed `mini.deps`" | redraw')
+end
+require("mini.deps").setup({ path = { package = path_package } })
+require("plugins")
+
+-- experimental Lua module loader
+vim.loader.enable()
+
+-- =============================================================================
+
 print("welcome back!")
